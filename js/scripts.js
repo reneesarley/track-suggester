a03417d93932b5e2cb3b1a06c2355322d43efc5c
Renee Sarley <reneesarley@gmail.com>
2018-06-01 12:05:47 -0700

    refactor to create function check for track class update

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,9 @@ $(function(){
 
 
     var currentValString = ""
+    function updateClasses(){};
+
+
   //on click function for any input
   $("input").click(function(){
     var currentAs
@@ -22,6 +25,18 @@ $(function(){
     var rubyNoCol
     var cssYesCol
     var cssNoCol
+    function updateClasses(current, yesCol, noCol, trackName){
+      alert("this function is working" + current);
+      if(current > 0){
+        yesCol = "col-xs-" + current;
+        noCol = "col-xs-" + (10-current);
+        $("#" + trackName + "Yes").attr('class', yesCol);
+        $("#" + trackName + "No").attr('class', noCol);
+        alert(trackName + " classes updated")
+      }else{
+        alert("no updates" + trackName);
+        }
+    };
     currentValString = currentValString + $(this).val();
     alert(currentValString);
     //get letter counts
@@ -32,23 +47,24 @@ $(function(){
     //add letter value to class var
     cssYesCol = "col-xs-" + currentBs;
     cssNoCol = "col-xs-" + (10-currentBs);
+    updateClasses(currentAs, rubyYesCol, rubyNoCol, "ruby");
     //add letter value to class var and class list for tracks if currentCount over 0
-    if(currentAs > 0){
-      rubyYesCol = "col-xs-" + currentAs;
-      rubyNoCol = "col-xs-" + (10-currentAs);
-      $("#rubyYes").attr('class', rubyYesCol);
-      $("#rubyNo").attr('class', rubyNoCol);
-    }else{
-      alert("no updates to As");
-      }
-    if(currentBs > 0){
-      cssYesCol = "col-xs-" + currentBs;
-      cssNoCol = "col-xs-" + (10-currentBs);
-      $("#cssYes").attr('class', cssYesCol);
-      $("#cssNo").attr('class', cssNoCol);
-    }else{
-      alert("no updates to Bs");
-      }
+    // if(currentAs > 0){
+    //   rubyYesCol = "col-xs-" + currentAs;
+    //   rubyNoCol = "col-xs-" + (10-currentAs);
+    //   $("#rubyYes").attr('class', rubyYesCol);
+    //   $("#rubyNo").attr('class', rubyNoCol);
+    // }else{
+    //   alert("no updates to As");
+    //   }
+    // if(currentBs > 0){
+    //   cssYesCol = "col-xs-" + currentBs;
+    //   cssNoCol = "col-xs-" + (10-currentBs);
+    //   $("#cssYes").attr('class', cssYesCol);
+    //   $("#cssNo").attr('class', cssNoCol);
+    // }else{
+    //   alert("no updates to Bs");
+    //   }
 
   });
 
