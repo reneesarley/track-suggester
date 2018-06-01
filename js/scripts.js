2842cce9de9a76e64d8bcb1ea312eb27c13f8a2b
Renee Sarley <reneesarley@gmail.com>
2018-06-01 09:33:09 -0700

    add submit button html and initial js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(function(){
 console.log("this is working");
 
+  $("#resetButton").click(function() {
+    $(".ourForm").trigger("reset");
+  });
+
+  $("#trackForm").submit(function(event) {
+    alert("submit button is working");
+
+    event.preventDefault();
+  });
 
 
 });
