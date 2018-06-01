0fc7dad2eb502cac3ea02bcf303021b26452b6aa
Renee Sarley <reneesarley@gmail.com>
2018-06-01 11:24:43 -0700

    add test variables and alerts for creating class names

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,23 @@ $(function(){
     var currentAs
     var currentBs
     var currentCs
+    var rubyYesClass
+    var rubyNoClass
     currentValString = currentValString + $(this).val();
     alert(currentValString);
+    //get letter counts
     currentAs = currentValString.split("a").length - 1;
     currentBs = currentValString.split("b").length - 1;
     currentCs = currentValString.split("c").length - 1;
     alert("a: " + currentAs + " b: " + currentBs + " c: " + currentCs);
+    //add letter value to class var
+    rubyYesClass = "col-md-" + currentAs;
+    rubyNoClass = "col-md-" + (10-currentAs);
+    alert(rubyYesClass);
+    alert(rubyNoClass);
+    //update class list for
+
+
   });
 
 
