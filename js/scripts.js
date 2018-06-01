10d6160b87899515f60c160ac854f6cd9772c951
Renee Sarley <reneesarley@gmail.com>
2018-06-01 12:16:55 -0700

    refactor updateClasses function to remove current argument

--- js/scripts.js
+++ js/scripts.js
@@ -18,18 +18,18 @@ $(function(){
 
   //on click function for any input
   $("input").click(function(){
-    var currentAs
+    var rubyCurrent
     var currentBs
     var currentCs
     var rubyYesCol
     var rubyNoCol
     var cssYesCol
     var cssNoCol
-    function updateClasses(current, yesCol, noCol, trackName){
-      alert("this function is working" + current);
-      if(current > 0){
-        yesCol = "col-xs-" + current;
-        noCol = "col-xs-" + (10-current);
+    function updateClasses(yesCol, noCol, trackName){
+      alert(trackName);
+      if(eval(trackName + "Current") > 0){
+        yesCol = "col-xs-" + eval(trackName + "Current");
+        noCol = "col-xs-" + (10- eval(trackName + "Current"));
         $("#" + trackName + "Yes").attr('class', yesCol);
         $("#" + trackName + "No").attr('class', noCol);
         alert(trackName + " classes updated")
@@ -40,14 +40,14 @@ $(function(){
     currentValString = currentValString + $(this).val();
     alert(currentValString);
     //get letter counts
-    currentAs = currentValString.split("a").length - 1;
+    rubyCurrent = currentValString.split("a").length - 1;
     currentBs = currentValString.split("b").length - 1;
     currentCs = currentValString.split("c").length - 1;
-    alert("a: " + currentAs + " b: " + currentBs + " c: " + currentCs);
+    alert("a: " + rubyCurrent + " b: " + currentBs + " c: " + currentCs);
     //add letter value to class var
     cssYesCol = "col-xs-" + currentBs;
     cssNoCol = "col-xs-" + (10-currentBs);
-    updateClasses(currentAs, rubyYesCol, rubyNoCol, "ruby");
+    updateClasses(rubyYesCol, rubyNoCol, "ruby");
     //add letter value to class var and class list for tracks if currentCount over 0
     // if(currentAs > 0){
     //   rubyYesCol = "col-xs-" + currentAs;
